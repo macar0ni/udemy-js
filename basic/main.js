6aef10bd7134f5c7655d0e04b5103f0585e32fd3
macar0ni <riman3939@gmail.com>
2025-04-20 15:39:04 +0900

    string型の3つの宣言方法
    文字列の数字をinteger型へ変換

--- basic/main.js
+++ basic/main.js
@@ -1,3 +1,19 @@
+const title = {
+  printTitle(chapNo) {
+    console.log(
+      '-------------------------------chapter' +
+        chapNo +
+        '-------------------------------'
+    );
+  },
+};
+
+const c = {
+  l(tmp) {
+    console.log(tmp);
+  },
+};
+
 let count = 0;
 console.log(count);
 count = 30;
@@ -17,3 +33,40 @@ result += 1;
 result++;
 // これは+1した後のresultを返す
 ++result;
+
+// 17
+// データの型
+// number型 integer float
+// string型
+// コード実行時に型を決めることを動的型付け、実行前に決めることを静的型付けと呼ぶ
+// jsは動的型付け
+
+// 18
+title.printTitle(18);
+// string型 3つの宣言方法
+// シングル、ダブル、バッククヲーテーション
+userName = 'macaroni';
+string = 'hello' + userName;
+
+string = `hello ${userName}`;
+console.log(string);
+
+string = 'hello\nsomeName';
+console.log(string);
+string = `hello
+someName`;
+console.log(string);
+
+title.printTitle(19);
+console.log('10' + 10);
+
+title.printTitle(20);
+const userNum = '10';
+let calcResult = Number(userNum) + 10;
+c.l(calcResult);
+calcResult = parseInt(userNum) + 10;
+c.l(calcResult);
+calcResult = parseFloat(userNum) + 10;
+c.l(calcResult);
+calcResult = +userNum + 10;
+c.l(calcResult);
